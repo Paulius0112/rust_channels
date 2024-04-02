5b3cf2a52760e7e95a4f16c6e8f36e027922ee94
Paulius Gasp <paulius.gasp@gmail.com>
2024-04-02 16:55:58 +0300

    buffer for receiver

--- rs_channels/src/lib.rs
+++ rs_channels/src/lib.rs
@@ -2,65 +2,106 @@ use std::sync::{Arc, Condvar, Mutex};
 use std::collections::VecDeque;
 
 
+
 pub struct Sender<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
 }
 
 impl<T> Clone for Sender<T> {
     fn clone(&self) -> Self {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders += 1;
+        drop(inner);
+
         Sender {
-            inner: Arc::clone(&self.inner),
+            shared: Arc::clone(&self.shared),
+
         }
     }
 } 
 
+impl<T> Drop for Sender<T> {
+    fn drop(&mut self) {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders -= 1;
+        let was_last = inner.senders == 0;
+        drop(inner);
+        if was_last {
+            self.shared.available.notify_one();
+
+        }
+    }
+}
+
 pub struct Receiver<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
+    buffer: VecDeque<T>,
 }
 
 impl<T> Sender<T> {
     pub fn send(&mut self, t: T) {
-        let mut queue = self.inner.queue.lock().unwrap();
-        queue.push_back(t);
-        drop(queue);
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.queue.push_back(t);
+        drop(inner);
         // Wake up the receiver after dropping the lock
-        self.inner.available.notify_one();
+        self.shared.available.notify_one();
     }
 }
 
 impl<T> Receiver<T> {
-    pub fn recv(&mut self) -> T {
-        let mut queue = self.inner.queue.lock().unwrap();
+    pub fn recv(&mut self) -> Option<T> {
+        if let Some(t) = self.buffer.pop_front() {
+            return Some(t);
+        }
+
+        let mut inner = self.shared.inner.lock().unwrap();
         loop {
-            match queue.pop_front() {
-                Some(t) => return t, 
+            match inner.queue.pop_front() {
+                Some(t) => {
+                    if !inner.queue.is_empty() {
+                        // Swap memory to not take the lock on every receive
+                        std::mem::swap(&mut self.buffer, &mut inner.queue);
+                    }
+                    return Some(t);
+                }
+                    
+                None if inner.senders == 0 => return None,
                 None => { 
-                    queue = self.inner.available.wait(queue).unwrap();
+                    inner = self.shared.available.wait(inner).unwrap();
                 }
             }
         }
-        
     }
 }
 
 struct Inner<T> {
-    queue: Mutex<VecDeque<T>>,
+    queue: VecDeque<T>,
+    senders: usize,
+}
+
+struct Shared<T> {
+    inner: Mutex<Inner<T>>,
     available: Condvar,
 }
 
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     let inner = Inner {
-        queue: Mutex::default(),
+        queue: VecDeque::default(),
+        senders: 1,
+    };
+    let shared = Shared {
+        inner: Mutex::new(inner),
         available: Condvar::new(),
     };
 
-    let inner = Arc::new(inner);
+    let shared = Arc::new(shared);
     (
         Sender {
-            inner: inner.clone()
+            shared: shared.clone()
         }, 
         Receiver {
-            inner: inner.clone()
+            shared: shared.clone(),
+            buffer: VecDeque::default(),
         },
     )
 }
@@ -70,10 +111,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn ping_ping() {
+    fn ping_pong() {
         let (mut tx, mut rx) = channel();
         tx.send(42);
-        assert_eq!(rx.recv(), 42);
+        assert_eq!(rx.recv(), Some(42));
+    }
+
+    #[test]
+    fn closed() {
+        let (tx, mut rx) = channel::<()>();
+        drop(tx);
+        assert_eq!(rx.recv(), None);
+    }
+
+    #[test]
+    fn closed_rx() {
+        let (mut tx, rx) = channel();
+        drop(rx);
+        tx.send(42);
     }
 
 }
